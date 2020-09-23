ad4d87b6d737ef4749a781d842d7f130df0a2f9f
Adi Iyengar <aditya7iyengar@gmail.com>
2020-09-23 19:52:48 -0400

    Add tests for error view

--- apps/bifrost/test/bifrost_web/views/error_view_test.exs
+++ apps/bifrost/test/bifrost_web/views/error_view_test.exs
@@ -1,6 +1,8 @@
 defmodule BifrostWeb.ErrorViewTest do
   use BifrostWeb.ConnCase, async: true
 
+  @module BifrostWeb.ErrorView
+
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
@@ -10,5 +12,12 @@ defmodule BifrostWeb.ErrorViewTest do
 
   test "renders 500.html" do
     assert render_to_string(BifrostWeb.ErrorView, "500.html", []) == "Internal Server Error"
+  end
+
+  describe "template_not_found/2" do
+    test "works as expected" do
+      template = "404.html"
+      assert @module.template_not_found(template, %{}) == "Not Found"
+    end
   end
 end
