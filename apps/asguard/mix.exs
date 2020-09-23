65f3c43a6e9a4a1b6b94f45b8cd90497f9c7ec5d
Adi Iyengar <aditya7iyengar@gmail.com>
2020-09-23 18:27:35 -0400

    Add consistent coveralls config

--- apps/asguard/mix.exs
+++ apps/asguard/mix.exs
@@ -13,7 +13,14 @@ defmodule Asguard.MixProject do
       deps_path: "../../deps",
       elixir: @elixir,
       lockfile: "../../mix.lock",
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
       version: @version
     ]
   end
@@ -26,7 +33,8 @@ defmodule Asguard.MixProject do
 
   defp deps do
     [
-      {:elixir_uuid, "~> 1.2.1"}
+      {:elixir_uuid, "~> 1.2.1"},
+      {:excoveralls, "~> 0.13.2", only: :test},
     ]
   end
 end
