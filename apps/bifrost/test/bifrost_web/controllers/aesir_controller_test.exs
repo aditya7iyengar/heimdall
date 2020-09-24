f8c6fca0c807a0749897c7cab0a14f0a78439b93
Adi Iyengar <aditya7iyengar@gmail.com>
2020-09-23 20:25:15 -0400

    Format code (`mix format`)

--- apps/bifrost/test/bifrost_web/controllers/aesir_controller_test.exs
+++ apps/bifrost/test/bifrost_web/controllers/aesir_controller_test.exs
@@ -19,11 +19,12 @@ defmodule BifrostWeb.AesirControllerTest do
 
   describe "create/2" do
     test "adds aesir to Asguard and redirects to root", %{auth_conn: conn} do
-      conn = post(
-        conn,
-        Routes.aesir_path(conn, :create),
-        aesir: @valid_aesir_params
-      )
+      conn =
+        post(
+          conn,
+          Routes.aesir_path(conn, :create),
+          aesir: @valid_aesir_params
+        )
 
       assert html_response(conn, 302) =~ "redirected"
       assert conn.private.plug_session["phoenix_flash"]["info"] =~ "Inserted"
@@ -37,11 +38,12 @@ defmodule BifrostWeb.AesirControllerTest do
       # This is to make it stop
       :ok = GenServer.stop(Asguard)
 
-      conn = post(
-        conn,
-        Routes.aesir_path(conn, :create),
-        aesir: @valid_aesir_params
-      )
+      conn =
+        post(
+          conn,
+          Routes.aesir_path(conn, :create),
+          aesir: @valid_aesir_params
+        )
 
       assert html_response(conn, 302) =~ "redirected"
       assert conn.private.plug_session["phoenix_flash"]["error"] =~ "Error"
