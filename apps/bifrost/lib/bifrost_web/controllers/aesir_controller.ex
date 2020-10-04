39b7cddfab77536e1f36dc92d8036542ef4dda39
Adi Iyengar <aditya7iyengar@gmail.com>
2020-10-03 22:12:45 -0400

    Fix bug related to max_attempts (Thanks browser tests)

--- apps/bifrost/lib/bifrost_web/controllers/aesir_controller.ex
+++ apps/bifrost/lib/bifrost_web/controllers/aesir_controller.ex
@@ -25,10 +25,7 @@ defmodule BifrostWeb.AesirController do
       |> Map.fetch!("ttl")
       |> ttl_from_params()
 
-    max_attempts =
-      aesir_params
-      |> Map.fetch!("max_attempts")
-      |> max_attempts_from_params()
+    max_attempts = Map.get(aesir_params, "max_attempts", :infinite)
 
     [
       Map.fetch!(aesir_params, "raw"),
@@ -47,7 +44,4 @@ defmodule BifrostWeb.AesirController do
 
   defp ttl_from_params(integer) when is_integer(integer), do: integer
   defp ttl_from_params(str) when is_binary(str), do: String.to_integer(str)
-
-  defp max_attempts_from_params(integer) when is_integer(integer), do: integer
-  defp max_attempts_from_params("infinite"), do: :infinite
 end
