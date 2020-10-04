fb6aecef011d6e90551f2ba0eb1946535dfca420
Adi Iyengar <aditya7iyengar@gmail.com>
2020-10-04 13:17:56 -0400

    Beautify JS test files

--- browser-tests/cypress/integration/decryption_cases.js
+++ browser-tests/cypress/integration/decryption_cases.js
@@ -10,64 +10,68 @@ const username = Cypress.env('BIFROST_USER');
 const password = Cypress.env('BIFROST_PASSWORD');
 
 describe('Up and Running', () => {
-  beforeEach(() => {
-    cy.visit(`http://localhost:${port}/aesirs/new`, {
-     auth: {
-        username: username,
-        password: password
-      }
+    beforeEach(() => {
+        cy.visit(`http://localhost:${port}/aesirs/new`, {
+            auth: {
+                username: username,
+                password: password
+            }
+        })
+
+        cy.get('input[name="aesir[raw]"]').invoke('val', 'Raw value')
+
+        cy.get('input[name="aesir[key]"]').invoke('val', 'secret')
+        // Give a few milliseconds for the websocket to do its magic
+        cy.wait(100)
+
+        cy.get('input[name="aesir[description]"]').invoke('val', 'other description')
+
+        cy.get('input[name="aesir[max_attempts]"]').invoke('val', 1)
+
+        cy.get('button[type=submit]').click()
+
+        // On the main page, search for aesir's description
+        cy.get('input[type=text]').invoke('val', 'other')
+
+        // Give a few milliseconds for the websocket to do its magic
+        cy.wait(100)
+
+        cy.get('form').submit()
+
+        // Visit aesir link
+        cy.get('a[class="aesir-link"]', {
+            timeout: 1000
+        }).first().click()
+
+        // Give a few milliseconds for the websocket to do its magic
+        cy.wait(100)
     })
 
-    cy.get('input[name="aesir[raw]"]').invoke('val', 'Raw value')
+    it('tests successful decryption an aesir', () => {
+        // Decrypt the information
+        cy.get('input[type=password]').invoke('val', 'secret')
 
-    cy.get('input[name="aesir[key]"]').invoke('val', 'secret')
-    // Give a few milliseconds for the websocket to do its magic
-    cy.wait(100)
+        // Give a few milliseconds for the websocket to do its magic
+        cy.wait(100)
 
-    cy.get('input[name="aesir[description]"]').invoke('val', 'other description')
+        cy.get('form').submit()
 
-    cy.get('input[name="aesir[max_attempts]"]').invoke('val', 1)
+        // Get button text
+        cy.get('button[class="clippy"]', {
+            timeout: 1000
+        }).should('contain.text', 'Copy to clipboard')
+    })
 
-    cy.get('button[type=submit]').click()
+    it('tests unsuccessful decryption an aesir', () => {
+        // Try to Decrypt the information with wrong key
+        cy.get('input[type=password]').invoke('val', 'bad-secret')
 
-    // On the main page, search for aesir's description
-    cy.get('input[type=text]').invoke('val', 'other')
+        // Give a few milliseconds for the websocket to do its magic
+        cy.wait(100)
 
-    // Give a few milliseconds for the websocket to do its magic
-    cy.wait(100)
+        cy.get('form').submit()
 
-    cy.get('form').submit()
-
-    // Visit aesir link
-    cy.get('a[class="aesir-link"]', { timeout: 1000 }).first().click()
-
-    // Give a few milliseconds for the websocket to do its magic
-    cy.wait(100)
-  })
-
-  it('tests successful decryption an aesir', () => {
-    // Decrypt the information
-    cy.get('input[type=password]').invoke('val', 'secret')
-
-    // Give a few milliseconds for the websocket to do its magic
-    cy.wait(100)
-
-    cy.get('form').submit()
-
-    // Get button text
-    cy.get('button[class="clippy"]', { timeout: 1000 }).should('contain.text', 'Copy to clipboard')
-  })
-
-  it('tests unsuccessful decryption an aesir', () => {
-    // Try to Decrypt the information with wrong key
-    cy.get('input[type=password]').invoke('val', 'bad-secret')
-
-    // Give a few milliseconds for the websocket to do its magic
-    cy.wait(100)
-
-    cy.get('form').submit()
-
-    // Get button text
-    cy.get('blockquote').should('contain.text', 'Decryption attempts remaining: 0')
-  })
+        // Get button text
+        cy.get('blockquote').should('contain.text', 'Decryption attempts remaining: 0')
+    })
 })
--- browser-tests/cypress/integration/up_and_running_spec.js
+++ browser-tests/cypress/integration/up_and_running_spec.js
@@ -10,57 +10,61 @@ const username = Cypress.env('BIFROST_USER');
 const password = Cypress.env('BIFROST_PASSWORD');
 
 describe('Up and Running', () => {
-  it('visits the root page with http basic auth', () => {
-    cy.visit(`http://localhost:${port}`, {
-     auth: {
-        username: username,
-        password: password
-      }
-    })
-  })
-
-  it('tests the complete life cycle of an aesir', () => {
-    cy.visit(`http://localhost:${port}/aesirs/new`, {
-     auth: {
-        username: username,
-        password: password
-      }
+    it('visits the root page with http basic auth', () => {
+        cy.visit(`http://localhost:${port}`, {
+            auth: {
+                username: username,
+                password: password
+            }
+        })
     })
 
-    cy.get('input[name="aesir[raw]"]').invoke('val', 'Raw value')
+    it('tests the complete life cycle of an aesir', () => {
+        cy.visit(`http://localhost:${port}/aesirs/new`, {
+            auth: {
+                username: username,
+                password: password
+            }
+        })
 
-    cy.get('input[name="aesir[key]"]').invoke('val', 'secret')
-    // Give a few milliseconds for the websocket to do its magic
-    cy.wait(100)
+        cy.get('input[name="aesir[raw]"]').invoke('val', 'Raw value')
 
-    cy.get('input[name="aesir[description]"]').invoke('val', 'some description')
+        cy.get('input[name="aesir[key]"]').invoke('val', 'secret')
+        // Give a few milliseconds for the websocket to do its magic
+        cy.wait(100)
+
+        cy.get('input[name="aesir[description]"]').invoke('val', 'some description')
 
 
-    cy.get('button[type=submit]').click()
+        cy.get('button[type=submit]').click()
 
-    // On the main page, search for aesir's description
-    cy.get('input[type=text]').invoke('val', 'some')
+        // On the main page, search for aesir's description
+        cy.get('input[type=text]').invoke('val', 'some')
 
-    // Give a few milliseconds for the websocket to do its magic
-    cy.wait(100)
+        // Give a few milliseconds for the websocket to do its magic
+        cy.wait(100)
 
-    cy.get('form').submit()
+        cy.get('form').submit()
 
-    // Visit aesir link
-    cy.get('a[class="aesir-link"]', { timeout: 1000 }).first().click()
+        // Visit aesir link
+        cy.get('a[class="aesir-link"]', {
+            timeout: 1000
+        }).first().click()
 
-    // Give a few milliseconds for the websocket to do its magic
-    cy.wait(100)
+        // Give a few milliseconds for the websocket to do its magic
+        cy.wait(100)
 
-    // Decrypt the information
-    cy.get('input[type=password]').invoke('val', 'secret')
+        // Decrypt the information
+        cy.get('input[type=password]').invoke('val', 'secret')
 
-    // Give a few milliseconds for the websocket to do its magic
-    cy.wait(100)
+        // Give a few milliseconds for the websocket to do its magic
+        cy.wait(100)
 
-    cy.get('form').submit()
+        cy.get('form').submit()
 
-    // Get button text
-    cy.get('button[class="clippy"]', { timeout: 1000 }).should('contain.text', 'Copy to clipboard')
-  })
+        // Get button text
+        cy.get('button[class="clippy"]', {
+            timeout: 1000
+        }).should('contain.text', 'Copy to clipboard')
+    })
 })
